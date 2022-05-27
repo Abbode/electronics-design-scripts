d91a62fd320b142eb7d2ba78bd1da5915bc28b25
Abd AlHaleem <abdalhaleembakkor@gmail.com>
2022-05-28 03:04:43 +0400

    modified:   reactance calculator.cpp 	added calculating capacitance and inductors to get certian impedance

--- reactance calculator.cpp
+++ reactance calculator.cpp
@@ -5,27 +5,53 @@ using namespace std;
 
 int main ()
 {
-	float f0,componentValue,pi=3.141592653589793238462643,X;
+	float f0,componentValue,X;
+	const float pi=3.141592653589793238462643;
 	char componentType;
+	int E;
 	system("clear");
-	cout<<"Reactance calculator"<<endl<<"please note that garbage in garbage out"<<endl;
+	cout<<"Reactance calculator"<<endl<<"please note that garbage in garbage out, 1e3=1k and 1e-3=1m"<<endl;
+	START:
 	cout<<"enter frequency in Hertz"<<endl;
 	cin>>f0;
-	cout<<"enter C for capacitor or L for inductor"<<endl;
+	cout<<"enter component type [C for capacitor or L for inductor]"<<endl;
 	cin>>componentType;
-	if (componentType == 'C' | componentType == 'c' )
+	cout<<"enter 1 to calculate reactance or 2 to calculate capacitance/inductance"<<endl;
+	cin>>E;
+	if (E==1)
 	{
 		cout<<"enter "<<componentType<<" value:"<<endl;
 		cin>>componentValue;
-		X=1/(2*pi*f0*componentValue);
+		if (componentType == 'C' | componentType == 'c' )
+		{
+			X=1/(2*pi*f0*componentValue);
+		}
+		else if (componentType == 'L' | componentType == 'l' )
+		{
+			X=2*pi*f0*componentValue;
+		}
+		else cout<<"garbage"<<endl;
+		cout<<endl<<"Reactance is:  "<<X<<"  Ohms"<<endl<<endl;
 	}
-	else if (componentType == 'L' | componentType == 'l' )
+	else if (E==2)
 	{
-		cout<<"enter "<<componentType<<" value:"<<endl;
-		cin>>componentValue;
-		X=2*pi*f0*componentValue;
+		cout<<"enter the impedance value you want to get in Ohms"<<endl;
+		cin>>X;
+		if (componentType == 'C' | componentType == 'c' )
+		{
+			componentValue=1/(2*pi*f0*X);
+			cout<<"capacitance C = "<<componentValue<<" F"<<endl;
+		}
+		else if (componentType == 'L' | componentType == 'l' )
+		{
+			componentValue=X/(2*pi*f0);
+			cout<<"inductance L = "<<componentValue<<" H"<<endl;
+		}
 	}
 	else cout<<"garbage"<<endl;
-	cout<<endl<<"Reactance is:  "<<X<<"  Ohms"<<endl<<endl;
+	cout<<endl<<"are you finished? (type 1 for yes or 0 to start another calculation)"<<endl;
+	cin>>E;
+	if (E==0)
+	goto START;
 	return 0;
 }
